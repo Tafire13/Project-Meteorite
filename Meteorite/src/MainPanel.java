cf4a4c89043e098d3079060188385a1e976c2840
Tafire13 <67011212063@msu.ac.th>
2025-09-21 18:52:49 +0700

    update

--- Meteorite/src/MainPanel.java
+++ Meteorite/src/MainPanel.java
@@ -7,13 +7,18 @@ import java.io.File;
 
 public class MainPanel extends JPanel {
     Image Meteorite1 = Toolkit.getDefaultToolkit().createImage(System.getProperty("user.dir")+ File.separator+"src"+ File.separator+"Meteorite1.png");
+    Image Meteorite2 = Toolkit.getDefaultToolkit().createImage(System.getProperty("user.dir")+ File.separator+"src"+ File.separator+"Meteorite2.png");
+    Image Meteorite3 = Toolkit.getDefaultToolkit().createImage(System.getProperty("user.dir")+ File.separator+"src"+ File.separator+"Meteorite3.png");
     int []meteorite1X = new int[5];
     int []meteorite1Y = new int[5];
+    Image[] Meteorite ={
+            Meteorite1, Meteorite2,Meteorite3,
+    };
     MainPanel() {
         setBackground(Color.black);
         for (int i = 0; i < meteorite1X.length; i++) {
             meteorite1X[i] = (int)(Math.random()*740);
-            meteorite1Y[i] = (int)(Math.random()*640);
+            meteorite1Y[i] = (int)(Math.random()*540);
         }
     }
 
@@ -21,7 +26,8 @@ public class MainPanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         for (int i = 0; i < meteorite1X.length; i++) {
-            g.drawImage(Meteorite1, meteorite1X[i], meteorite1Y[i],50,50, this);
+            int randomIndex = (int)(Math.random() * Meteorite.length);
+            g.drawImage(Meteorite[randomIndex], meteorite1X[i], meteorite1Y[i],50,50, this);
         }
     }
 }
