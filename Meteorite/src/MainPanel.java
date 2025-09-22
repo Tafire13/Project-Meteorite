b18420616acd1ab96072562df68b8de56fca81c1
Tafire13 <67011212063@msu.ac.th>
2025-09-22 20:22:40 +0700

    updateMeteoriteMove

--- Meteorite/src/MainPanel.java
+++ Meteorite/src/MainPanel.java
@@ -5,29 +5,49 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.io.File;
 
-public class MainPanel extends JPanel {
+public class MainPanel extends JPanel implements Runnable {
     Image Meteorite1 = Toolkit.getDefaultToolkit().createImage(System.getProperty("user.dir")+ File.separator+"src"+ File.separator+"Meteorite1.png");
     Image Meteorite2 = Toolkit.getDefaultToolkit().createImage(System.getProperty("user.dir")+ File.separator+"src"+ File.separator+"Meteorite2.png");
     Image Meteorite3 = Toolkit.getDefaultToolkit().createImage(System.getProperty("user.dir")+ File.separator+"src"+ File.separator+"Meteorite3.png");
     int []meteorite1X = new int[5];
     int []meteorite1Y = new int[5];
     Image[] Meteorite ={
-            Meteorite1, Meteorite2,Meteorite3,
+            Meteorite1, Meteorite2,Meteorite3
     };
+    int[] indexMeteorite = new int[5];
     MainPanel() {
         setBackground(Color.black);
         for (int i = 0; i < meteorite1X.length; i++) {
             meteorite1X[i] = (int)(Math.random()*740);
             meteorite1Y[i] = (int)(Math.random()*540);
+            indexMeteorite[i] = (int)(Math.random()*Meteorite.length);
         }
+        Thread thread = new Thread(this);
+        thread.start();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         for (int i = 0; i < meteorite1X.length; i++) {
-            int randomIndex = (int)(Math.random() * Meteorite.length);
-            g.drawImage(Meteorite[randomIndex], meteorite1X[i], meteorite1Y[i],50,50, this);
+            g.drawImage(Meteorite[indexMeteorite[i]], meteorite1X[i], meteorite1Y[i],50,50, this);
+
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            for (int i = 0; i < meteorite1X.length; i++) {
+                meteorite1X[i] += (int)(Math.random()*11) - 5;
+                meteorite1Y[i] += (int)(Math.random()*11) - 5;
+            }
+            repaint();
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
